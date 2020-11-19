d870e163c9f461545c0895335488083651b1ba62
Nightmare-MY <906262255@qq.com>
2020-11-19 20:37:02 +0800

    Create test.dart

---
+++ lib/src/test.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+
+void main() {
+  // print('\n'.codeUnits);
+  // print(utf8.decode(<int>[27]));
+  // print(utf8.decode(<int>[91]));
+  print(utf8.decode(<int>[63]));
+  print(utf8.encode('D'));
+  print('${'a' * 75}\x0dbbb\n');
+}
