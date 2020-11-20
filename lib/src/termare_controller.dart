8bed00543b1685054df0530275285b9e954e99cc
Nightmare-MY <906262255@qq.com>
2020-11-20 20:00:38 +0800

    Update termare_controller.dart

--- lib/src/termare_controller.dart
+++ lib/src/termare_controller.dart
@@ -9,61 +9,59 @@ import 'theme/term_theme.dart';
 /// Flutter Controller 的思想
 /// 一个TermView对应一个 Controller
 /// 在 Controller 被初始化的时候，底层终端已经被初始化了。
-class TermareStyle {
-  const TermareStyle();
-  final bool showCursor = true;
-}
 
 class TermareController with Observable {
-  final Map<String, String> environment;
-  final TermareStyle termareStyle;
-  bool dirty = false;
-  String out = '';
-  TermareController(
-      {this.termareStyle = const TermareStyle(), this.environment}) {
+  TermareController({
+    this.theme = TermareStyles.termux,
+    this.environment,
+  }) {
     unixPthC = UnixPtyC(environment: environment);
   }
+  final Map<String, String> environment;
 
-  TermTheme theme = TermThemes.termux;
+  /// 通过这个值来判断终端是否需要刷新
+  /// 每次从 pty 中读出数据的时候会将当前终端页标记为脏，在下一帧页终端就会进执行刷新
+  bool dirty = false;
+  String out = '';
+  final TermareStyle theme;
   UnixPtyC unixPthC;
-  void write(String data) => unixPthC.write(data);
-  String read() => unixPthC.read();
-  StringBuffer buffer = StringBuffer();
-  Future<void> defineTermFunc(String func, {String tmpFilePath}) async {
-    tmpFilePath ??=
-        '${PlatformUtil.getFilsePath(await PlatformUtil.getPackageName())}/1234567';
-    print('定义函数中...--->$tmpFilePath');
-    String cache = '';
-    // OutCallback callback = (String output) {
-    //   cache = output;
-    //   print('output=====>$output');
-    // };
-    // addListener(callback);
-    // print('创建临时脚本...');
-    File tmpFile = File('$tmpFilePath');
 
+  bool showCursor = true;
+
+  /// 直接指向 pty write 函数
+  void write(String data) => unixPthC.write(data);
+
+  /// 指向 pty read 函数
+  String read() => unixPthC.read();
+
+  Future<void> defineTermFunc(
+    String func, {
+    String tmpFilePath,
+  }) async {
+    tmpFilePath ??=
+        '${PlatformUtil.getFilsePath(await PlatformUtil.getPackageName())}/tmp';
+    print('定义函数中...--->$tmpFilePath');
+    final File tmpFile = File(tmpFilePath);
     await tmpFile.writeAsString(func);
-    // tmpFile.watch().listen((event) {
-    //   print('event--->>>>>$event');
-    // });
     print('创建临时脚本成功...->${tmpFile.path}');
     write(
-      "export AUTO=TRUE\n",
+      'export AUTO=TRUE\n',
     );
     write(
-      "source $tmpFilePath\n",
+      'source $tmpFilePath\n',
     );
     write(
-      "rm -rf $tmpFilePath\n",
+      'rm -rf $tmpFilePath\n',
     );
     while (true) {
-      bool exist = await tmpFile.exists();
+      final bool exist = await tmpFile.exists();
+      // 把不想被看到的代码读掉
       read();
       // print('read()->${read()}');
       if (!exist) {
         break;
       }
-      await Future.delayed(Duration(milliseconds: 100));
+      await Future<void>.delayed(const Duration(milliseconds: 100));
     }
   }
 }
