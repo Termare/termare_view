4c6e941f72a74b710f0850173d91cb48e5702dda
Nightmare-MY <906262255@qq.com>
2020-11-22 22:01:17 +0800

    Update test.dart

--- lib/src/test.dart
+++ lib/src/test.dart
@@ -1,12 +1,8 @@
 import 'dart:convert';
 
 void main() {
-  // print('\n'.codeUnits);
-  // print(utf8.decode(<int>[27]));
-  // print(utf8.decode(<int>[91]));
-  print(utf8.decode(<int>[63]));
-  print(utf8.encode('D'));
-  print('${'a' * 75}\x0dbbb\n');
-  print('\x7f'.codeUnits);
-  print(utf8.decode(<int>[128]));
+  print('s⣷s');
+  print('s啊s');
+  print(utf8.encode('⣷'));
+  print(utf8.encode('啊'));
 }
