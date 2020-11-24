c36885330a660a72932624b513ef64e0a51e25d2
Nightmare-MY <906262255@qq.com>
2020-11-24 19:05:28 +0800

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
 echo -n 解压中
-/usr/bin/env python3 "/Users/nightmare/Desktop/termare-space/termare/test/test.py" "sleep 3"
+/usr/bin/env python3 "/Users/nightmare/Desktop/termare-space/termare/test/test.py" "sleep 5"
 echo  解压完成
