e2951f3daa671d1d748313ccc6e1577820654697
Nightmare-MY <906262255@qq.com>
2020-11-29 13:50:08 +0800

    Update ssh.dart

--- example/lib/ssh.dart
+++ example/lib/ssh.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:dartssh/client.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:termare/termare.dart';
 
 const host = 'ssh://39.107.248.176:22';
@@ -29,12 +32,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TermareController controller = TermareController();
+  TermareController controller;
   SSHClient client;
 
   @override
   void initState() {
     super.initState();
+    final Size size = window.physicalSize;
+    print(size);
+    print(window.devicePixelRatio);
+    final double screenWidth = size.width / window.devicePixelRatio;
+    final double screenHeight = size.height / window.devicePixelRatio;
+    // 行数
+    final int row = screenHeight ~/ TermareStyles.termux.letterHeight;
+    // 列数
+    final int column = screenWidth ~/ TermareStyles.termux.letterWidth;
+    print('< row : $row column : $column>');
+    controller = TermareController(
+      environment: Platform.isIOS
+          ? {}
+          : {
+              'TERM': 'screen-256color',
+              'PATH': '/data/data/com.nightmare/files/usr/bin:' +
+                  Platform.environment['PATH'],
+            },
+      rowLength: row - 2,
+      columnLength: column - 2,
+    );
+
     connect();
   }
 
@@ -70,6 +95,9 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SafeArea(
         child: TermareView(
           controller: controller,
+          keyboardInput: (String data) {
+            client?.sendChannelData(Uint8List.fromList(utf8.encode(data)));
+          },
         ),
       ),
     );
