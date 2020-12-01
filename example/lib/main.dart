bd67bead9e82957e6b2d2633a283923cf38e80fd
Nightmare-MY <906262255@qq.com>
2020-12-01 08:17:07 +0800

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,61 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:termare/termare.dart';
-import 'package:termare_example/ssh.dart';
+
+import 'termare.dart';
 
 void main() {
-  runApp(SshMyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        // child: Material(
-        //   child: Column(
-        //     children: [
-        //       Text(
-        //         '测试字体 Test Font' * 3,
-        //         style: TextStyle(
-        //           fontFamily: 'packages/termare/UbuntuMono',
-        //           // fontWeight: FontWeight.bold,
-        //         ),
-        //       ),
-        //       Text(
-        //         '测试字体 Test Font' * 3,
-        //         style: TextStyle(
-        //           fontFamily: 'packages/termare/DroidSansMono',
-        //         ),
-        //       ),
-        //       Text(
-        //         '测试字体 Test Font' * 3,
-        //         style: TextStyle(
-        //           fontFamily: 'packages/termare/SourceCodePro',
-        //         ),
-        //       ),
-        //       Text(
-        //         '测试字体 Test Font' * 3,
-        //         style: TextStyle(
-        //           fontFamily: 'packages/termare/sarasa-mono-sc-bold',
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ),
-        child: TermarePty(),
-      ),
-    );
-  }
+  runApp(
+    MaterialApp(
+      home: Termare(),
+    ),
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: Colors.transparent,
+    systemNavigationBarDividerColor: Colors.transparent,
+  ));
 }
