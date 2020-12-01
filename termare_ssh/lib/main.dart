69ec0508b7e45ca9fad63573d299091c4e93f091
Nightmare-MY <906262255@qq.com>
2020-12-01 08:23:17 +0800

    Update main.dart

--- termare_ssh/lib/main.dart
+++ termare_ssh/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
