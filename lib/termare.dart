4261ab9bcee843297b3849c41e8f286694cfd125
Nightmare-MY <906262255@qq.com>
2020-12-01 08:22:29 +0800

    Update termare.dart

--- lib/termare.dart
+++ lib/termare.dart
@@ -1,18 +1,4 @@
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-export 'src/termare_pty.dart';
+// export '../example/lib/termare.dart';
 export 'src/termare_controller.dart';
 export 'src/termare_view.dart';
 export 'src/theme/term_theme.dart';
-
-class Termare {
-  // File().
-  static const MethodChannel _channel = const MethodChannel('termare');
-
-  // static Future<String> get platformVersion async {
-  //   final String version = await _channel.invokeMethod('getPlatformVersion');
-  //   return version;
-  // }
-}
