baa18c1c90f98f6aa45e0234fff09a9777502939
Nightmare-MY <906262255@qq.com>
2020-12-07 14:36:30 +0800

    Update main.dart

--- termare_pty/lib/main.dart
+++ termare_pty/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: TermarePty(),
+      home: const TermarePty(),
     );
   }
 }
