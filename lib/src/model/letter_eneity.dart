bf5dd8fe57e0cab3aa96d1df383bde219cd7f3a4
Nightmare-MY <906262255@qq.com>
2020-12-16 13:32:44 +0800

    Update letter_eneity.dart

--- lib/src/model/letter_eneity.dart
+++ lib/src/model/letter_eneity.dart
@@ -3,19 +3,21 @@ import 'package:termare_view/src/painter/model/position.dart';
 
 class LetterEntity {
   LetterEntity({
+    this.fontColorTag,
+    this.backgroundColorTag,
+    this.fontStyleTag,
     this.doubleWidth = false,
-    this.backgroundColor = Colors.transparent,
     @required this.content,
     @required this.letterWidth,
     @required this.letterHeight,
     @required this.position,
-    @required this.textStyle,
   });
   final String content;
   final double letterWidth;
   final double letterHeight;
   final Position position;
-  final TextStyle textStyle;
-  final Color backgroundColor;
   final bool doubleWidth;
+  final String fontColorTag;
+  final String backgroundColorTag;
+  final String fontStyleTag;
 }
