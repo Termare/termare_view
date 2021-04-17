b64427514e82badb18bc7c214f7f748f80cd291c
Nightmare-MY <906262255@qq.com>
2021-04-17 16:26:35 +0800

    Create debouncer.dart

---
+++ lib/src/utils/debouncer.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+/// This "function" class is the implementation of [debouncer()] Worker.
+/// It calls the function passed after specified [delay] parameter.
+/// Example:
+/// ```
+/// final delayed = Debouncer( delay: Duration( seconds: 1 )) ;
+/// print( 'the next function will be called after 1 sec' );
+/// delayed( () => print( 'called after 1 sec' ));
+/// ```
+class Debouncer {
+  Debouncer({this.delay});
+
+  final Duration delay;
+  Timer _timer;
+
+  void call(void Function() action) {
+    _timer?.cancel();
+    print('Debouncer cancel');
+    _timer = Timer(delay, action);
+  }
+
+  /// Notifies if the delayed call is active.
+  bool get isRunning => _timer?.isActive ?? false;
+
+  /// Cancel the current delayed call.
+  void cancel() => _timer?.cancel();
+}
