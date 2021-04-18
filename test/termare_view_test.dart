b05b79615aa8ca22c09e1668a9d8a77e8531d5a8
Nightmare-MY <906262255@qq.com>
2021-04-18 14:46:48 +0800

    Update termare_view_test.dart

--- test/termare_view_test.dart
+++ test/termare_view_test.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'dart:developer' as developer;
+
+void main() {
+  for (int i = 0; i < 256; i++) {
+    print('\x1b[48;5;$i\m$i     \x1b[0m');
+  }
+  developer.log('value', name: 'GETX');
+}
