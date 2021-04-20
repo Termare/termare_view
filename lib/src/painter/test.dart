9d7457f69a5f4ecfeb6bc9bd4c72aae371a16e20
Nightmare-MY <906262255@qq.com>
2021-04-20 19:13:21 +0800

    Update test.dart

--- lib/src/painter/test.dart
+++ lib/src/painter/test.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'dart:io';
 
 void main() {
-  stdout.write(utf8.decode([27, 107, 126, 27, 92]));
-  String a = stdin.readLineSync();
+  // stdout.write(utf8.decode([27, 107, 126, 27, 92]));
+  // String a = stdin.readLineSync();
+  print('� � \uFFFD'.codeUnits);
+  // print('\x1b'.codeUnits);
 }
