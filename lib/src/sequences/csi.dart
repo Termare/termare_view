c87d13caa6eeda17aec2beb36da57b63c499718f
Nightmare-MY <906262255@qq.com>
2021-05-20 19:45:54 +0800

    Update csi.dart

--- lib/src/sequences/csi.dart
+++ lib/src/sequences/csi.dart
@@ -601,7 +601,9 @@ class Csi {
   static void handle(TermareController controller, List<int> utf8CodeUnits) {
     final String currentChar = utf8.decode(utf8CodeUnits);
     if (csiSeqHandlerMap.containsKey(currentChar)) {
-      Log.d('current sequence -> $green$sequence $currentChar');
+      if (controller.enableLog) {
+        Log.d('current sequence -> $green$sequence $currentChar');
+      }
       final CsiHandler handler = csiSeqHandlerMap[currentChar]!;
       // 执行此次序列
       // 执行完清空
